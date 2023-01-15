bba35502ad9f7f1eb8dce4e6047aae8abc4fa233
Clint Hill <clint.hill@gmail.com>
2023-01-14 22:46:32 -0700

    Added eslint, to support lint check. Fixed existing lint errors.

--- test/test.js
+++ test/test.js
@@ -27,8 +27,8 @@ test('Get rolled up main file', async t => {
 
 test('cleanup', async t => {
   t.plan(1)
-  const public = join(workingDirectory, 'public')
+  const pub = join(workingDirectory, 'public')
   await sandbox.end()
-  const removed = await rm(public, { force: true, recursive: true })
+  await rm(pub, { force: true, recursive: true })
   t.pass('Shut down Sandbox')
 })
