962101692a01287bee35e167c1d120746c0ed8d6
Clint Hill <clint.hill@gmail.com>
2023-02-03 15:48:47 -0700

    CJS module setup that would fail without plugins.

--- test/rollup-mock/app/browser/index.mjs
+++ test/rollup-mock/app/browser/index.mjs
@@ -1,5 +1,8 @@
 import enhance from '@enhance/element'
 import MyElement from '../elements/my-element.mjs'
+import noDefault from "./no-default-export.js"
+
+noDefault("Should work even though it's CJS.")
 
 enhance('my-element', MyElement)
 
