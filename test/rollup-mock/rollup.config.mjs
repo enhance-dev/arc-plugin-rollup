17abc9e2d62e674bd732b47aae3dcf2464a19797
Clint Hill <clint.hill@gmail.com>
2023-02-03 15:47:50 -0700

    Setup the Rollup CommonJS plugin.

--- test/rollup-mock/rollup.config.mjs
+++ test/rollup-mock/rollup.config.mjs
@@ -1,5 +1,8 @@
+import commonjs from "@rollup/plugin-commonjs";
+
 export default {
   output: {
     intro: "Hello Rollup!"
-  }
+  },
+  plugins: [commonjs()] // This plugin should be included
 };
